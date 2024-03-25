2bfe0a1db7ced3ce2a3ad92c59b50bce5eae1dfe
Carlos Paredes <carlos@paredesrobles.com>
2024-03-25 00:15:31 -0300

    lint(): reformat to eslint

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import convertMarkdownToHtml from './utils/markdownParser.js';
 
 const addEventListeners = () => {
-    const markdownInput = document.getElementById('markdown-input');
-    const htmlOutput = document.getElementById('html-output');
+  const markdownInput = document.getElementById('markdown-input');
+  const htmlOutput = document.getElementById('html-output');
 
-    markdownInput.addEventListener('input', () => {
-        const markdown = markdownInput.value;
-        const html = convertMarkdownToHtml(markdown);
-        htmlOutput.innerHTML = html;
-    });
-}
+  markdownInput.addEventListener('input', () => {
+    const markdown = markdownInput.value;
+    const html = convertMarkdownToHtml(markdown);
+    htmlOutput.innerHTML = html;
+  });
+};
 
 addEventListeners();
