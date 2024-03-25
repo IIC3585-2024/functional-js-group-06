72a5edf9bd08107c9295eee0a7f1201c77ea2f59
Carlos Paredes <carlos@paredesrobles.com>
2024-03-24 23:54:13 -0300

    feat(constant): add markdown regex rules

---
+++ constants/regexExpr.js
@@ -0,0 +1,19 @@
+const regexPatterns = {
+    headers: /^(#{1,6})\s+(.*?)\s*$/gm,
+    blockquotes: /^(> )(.*)$/gm,
+    unorderedList: /^(\s*)[-+*]\s+(.*)$/gm,
+    orderedList: /^(\s*)\d+\.\s+(.*)$/gm,
+    bold: /(\*\*|__)(.*?)\1/g,
+    italics: /(\*|_)(.*?)\1/g,
+    strikethrough: /~~(.*?)~~/g,
+    links: /\[(.*?)\]\((.*?)\)/g,
+    images: /!\[(.*?)\]\((.*?)\)/g,
+    pre: /```([\s\S]*?)```/g,
+    code: /`([^`]+)`/g,
+    breakline: /\n\n/g,
+    quickLinks: /\<(.*?)\>/g,
+    unorderedListItems: /((?:\s*<li style="margin-left:.*em;">.*<\/li>\n?)+)/g,
+    orderedListItems: /((?:\s*<li style="margin-left:.*em;">.*<\/li>\n?)+)/g
+};
+
+export default regexPatterns;
